1e3bd34976b56f0ca291bec751e80c2d8fe2904a
Ala <alabny18@gmail.com>
2020-02-26 14:22:14 +0100

    Add printBoard method

--- src/com/pickurapps/Game.java
+++ src/com/pickurapps/Game.java
@@ -8,5 +8,15 @@ public class Game {
     public static int row, col;
     public static Scanner scanner = new Scanner(System.in);
 
-    
+    // prints the board (nicely formatted)
+    public static void printBoard() {
+        for (int i = 0; i < 3; i++){
+            System.out.println();
+            for (int j = 0; j < 3; j++){
+                if (j == 0)
+                    System.out.print("| ");
+                System.out.print(board[i][j] + " | ");
+            }
+        }
+    }
 }
