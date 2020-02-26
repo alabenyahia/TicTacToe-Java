63ec5253662867955bd82503e5f32aac920b3a00
Ala <alabny18@gmail.com>
2020-02-26 14:20:04 +0100

    Add class variables

--- src/com/pickurapps/Game.java
+++ src/com/pickurapps/Game.java
@@ -3,5 +3,10 @@ package com.pickurapps;
 import java.util.Scanner;
 
 public class Game {
+    public static char turn = 'X';
+    public static char[][] board = { {'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'} };
+    public static int row, col;
+    public static Scanner scanner = new Scanner(System.in);
+
     
 }
--- src/com/pickurapps/Main.java
+++ src/com/pickurapps/Main.java
@@ -3,6 +3,6 @@ package com.pickurapps;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
